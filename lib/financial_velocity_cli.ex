d8418b80540092c31f6bb0fce034e40ad67ad483
Bruce <brucepark0@gmail.com>
2018-02-22 21:26:51 -0800

    Step 5: Add CLI and output report

--- lib/financial_velocity_cli.ex
+++ lib/financial_velocity_cli.ex
@@ -14,5 +14,23 @@ defmodule FinancialVelocity.Cli do
   end
 
   defp output_report(opts) do
+    IO.puts "Your initial amount is #{principal(opts)}"
+    IO.puts "Your assumed percentage growth is #{percent(opts) * 100.0}"
+    IO.puts "Your assumed monthly savings is #{saving(opts)}"
+    IO.puts "==============="
+    IO.puts "Your final amount after #{years(opts)} year(s) will be:"
+    amount =
+      FinancialVelocity.calculate(principal(opts), percent(opts), saving(opts), years(opts))
+      |> format_amount
+    IO.puts amount
+  end
+
+  defp principal(opts), do: opts |> Keyword.get(:principal, 500.0e3)
+  defp percent(opts), do: opts |> Keyword.get(:percent, 0.1)
+  defp saving(opts), do: opts |> Keyword.get(:saving, 3000.0)
+  defp years(opts), do: opts |> Keyword.get(:years, 1)
+  defp format_amount(amount) do
+    # source: https://stackoverflow.com/questions/43536481/how-to-format-a-number-to-precision-in-elixir
+    "$" <> (:io_lib.format("~.2f", [amount]) |> IO.iodata_to_binary)
   end
 end

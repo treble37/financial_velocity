a0003493216154cf9f07d86b658557ce9f536534
Bruce <brucepark0@gmail.com>
2018-02-22 19:08:43 -0800

    Step 1: Initial mix application

---
+++ lib/financial_velocity.ex
@@ -0,0 +1,18 @@
+defmodule FinancialVelocity do
+  @moduledoc """
+  Documentation for FinancialVelocity.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FinancialVelocity.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/financial_velocity_test.exs
@@ -0,0 +1,8 @@
+defmodule FinancialVelocityTest do
+  use ExUnit.Case
+  doctest FinancialVelocity
+
+  test "greets the world" do
+    assert FinancialVelocity.hello() == :world
+  end
+end

0dc0cdf784c260fd64bc224ef890d03d3122f5fd
Bruce <brucepark0@gmail.com>
2018-02-22 20:14:59 -0800

    Step 3: Implementation of calculation algorithm

--- lib/financial_velocity.ex
+++ lib/financial_velocity.ex
@@ -4,15 +4,13 @@ defmodule FinancialVelocity do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FinancialVelocity.hello
-      :world
-
+  Compute your year over year gains
   """
-  def hello do
-    :world
+  @spec calculate(float(), float(), float(), integer()) :: float()
+  def calculate(initial_amount, percent, monthly_savings, 0),
+    do: initial_amount |> Float.round(2)
+  def calculate(initial_amount, percent, monthly_savings, years) do
+    (initial_amount * (percent + 1.0) + monthly_savings * 12)
+    |> calculate(percent, monthly_savings, years - 1)
   end
 end
